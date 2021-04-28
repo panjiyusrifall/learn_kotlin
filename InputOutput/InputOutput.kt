f66b1a356602cfed785f3672db98322f441407bd
Panji Yusrial <panjiyusrifall@gmail.com>
2021-04-28 22:30:39 +0700

    Input Output dengan Kotlin

---
+++ InputOutput/InputOutput.kt
@@ -0,0 +1,7 @@
+fun main() {
+    print ("Msukan Nama:")
+    val nama = readLine()
+    print ("Masukan Umur:")
+    val umur = readLine()
+    print ("Nama anda\t: $nama \nUmur anda\t: $umur\n")
+}
